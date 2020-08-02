8eef7eb96b2f5d39c4375ed7b308aee02dfed5c0
kent <silnith@gmail.com>
2020-08-01 20:57:07 -0700

    Add another module.

---
+++ thing-impl-a/src/main/java/org/silnith/example/osgi/thing/impl/a/ThingImpl.java
@@ -0,0 +1,17 @@
+package org.silnith.example.osgi.thing.impl.a;
+
+import org.osgi.service.component.annotations.Component;
+import org.silnith.example.osgi.thing.Thing;
+
+/**
+ * Implementation of Thing API using A.
+ */
+@Component
+public class ThingImpl implements Thing {
+
+    @Override
+    public Object doThing(final String input) {
+        return null;
+    }
+
+}
---
+++ thing-impl-a/src/main/java/org/silnith/example/osgi/thing/impl/a/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Implementation of Thing API using A.
+ */
+package org.silnith.example.osgi.thing.impl.a;
