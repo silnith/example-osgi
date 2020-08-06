a3b5b983f1e1c34fc969f6d31d564e83b172c844
kent <silnith@gmail.com>
2020-08-05 22:05:54 -0700

    Make Blueprint explicit with a bundle header.

--- thing-impl-blueprint/src/main/java/org/silnith/example/osgi/thing/impl/blueprint/package-info.java
+++ thing-impl-blueprint/src/main/java/org/silnith/example/osgi/thing/impl/blueprint/package-info.java
@@ -1,5 +1,8 @@
 /**
  * Blueprint implementation of Thing API.
  */
+@org.osgi.annotation.bundle.Headers({
+    @org.osgi.annotation.bundle.Header(name = "Bundle-Blueprint", value = "OSGI-INF/blueprint/*.xml")
+})
 @org.osgi.annotation.versioning.Version("1.0.0")
 package org.silnith.example.osgi.thing.impl.blueprint;
